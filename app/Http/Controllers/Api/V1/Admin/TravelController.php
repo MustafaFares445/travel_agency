ac458b9cfb1f0f9d930efb8c4bf8b1315b15dafb
Mustafa Fares <mustaphafares.dev@gmail.com>
2024-05-31 19:06:16 +0300

    TR-10 [FEAT] Add Travel create endpoint

---
+++ app/Http/Controllers/Api/V1/Admin/TravelController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\TravelRequest;
+use App\Http\Resources\TravelResource;
+use App\Models\Travel;
+use Illuminate\Http\Request;
+
+class TravelController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(TravelRequest $request)
+    {
+        $travel = Travel::create($request->validated());
+
+        return new TravelResource($travel);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Travel $travel)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Travel $travel)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Travel $travel)
+    {
+        //
+    }
+}
---
+++ app/Http/Requests/TravelRequest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class TravelRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'isPublic' => ['required' , 'boolean'],
+            'name' => ['required' , 'unique:travels'],
+            'description' => ['required'],
+            'numberOfDays' => ['required' , 'integer'],
+        ];
+    }
+
+    public function validated($key = null, $default = null)
+    {
+        return [
+            'is_public' => $this->isPublic,
+            'name' => $this->name,
+            'description' => $this->description,
+            'number_of_days' => $this->numberOfDays,
+        ];
+    }
+}
