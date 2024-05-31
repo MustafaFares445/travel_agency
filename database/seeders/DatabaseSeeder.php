79a10956abfb1f3abc2d1b4f9449a0d26cd14a40
Mustafa Fares <mustaphafares.dev@gmail.com>
2024-05-31 16:24:55 +0300

    TR-7 [FEAT] Add fake data for Travels & Tours Tables

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Tour;
+use App\Models\Travel;
+use Database\Factories\TourFactory;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -18,5 +21,7 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+       Tour::factory(10)->create();
     }
 }
