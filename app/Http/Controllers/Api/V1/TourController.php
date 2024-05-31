90b76ab66d295ddbec81b6fcbded90957240705f
Mustafa Fares <mustaphafares.dev@gmail.com>
2024-05-31 16:55:09 +0300

    TR-8 [FEAT] Add filter feature to Tour List endpoint

--- app/Http/Controllers/Api/V1/TourController.php
+++ app/Http/Controllers/Api/V1/TourController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\ToursListRequest;
 use App\Http\Resources\TourResource;
 use App\Models\Tour;
 use App\Models\Travel;
@@ -13,9 +14,21 @@ class TourController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index(Travel $travel)
+    public function index(Travel $travel , ToursListRequest $request)
     {
         $tours = $travel->tours()
+            ->when($request->priceFrom , function ($query) use ($request){
+                $query->where('price', '>=', $request->priceFrom * 100);
+            })
+            ->when($request->dateFrom , function ($query) use ($request){
+                $query->where('starting_date', '>=', $request->dateFrom);
+            })
+            ->when($request->dateTo , function ($query) use ($request){
+                $query->where('ending_date', '<=', $request->dateTo);
+            })
+            ->when($request->sortBy && $request->sortOrder , function ($query) use ($request){
+                $query->orderBy($request->sortBy , $request->sortOrder);
+            })
             ->orderBy('starting_date')
             ->paginate();
 
